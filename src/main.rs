856a710f7504b72b1a90d976df1917ebb70cedd0
Ivo Georgiev <ivo@strem.io>
2019-08-25 14:13:34 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use http::header::HeaderValue;
+use http::status::StatusCode;
+use tide::{Context, App, response, EndpointResult};
+use tide::middleware::{CorsMiddleware, CorsOrigin};
+use stremio_core::types::addons::{ResourceRef, ResourceResponse, Manifest, ManifestResource};
+use semver::Version;
+use std::str::FromStr;
+
+// @TODO another return type
+async fn handle_path(ctx: Context<Manifest>) -> EndpointResult {
+    let path = ctx.uri().path();
+    let parsed = ResourceRef::from_str(&path);
+    match (path, parsed) {
+        ("/manifest.json", _) => Ok(response::json(ctx.state())),
+        (_, Ok(p)) => {
+            dbg!(p);
+            let msg = ResourceResponse::Streams { streams: vec![] };
+            Ok(response::json(msg))
+        },
+        _ => Err(StatusCode::NOT_FOUND.into()),
+    }
+}
+
+fn main() {
+    let mut app = App::with_state(Manifest {
+        id: "org.test".into(),
+        name: "test".into(),
+        version: Version::new(1, 0, 0),
+        resources: vec![ManifestResource::Short("stream".into())],
+        types: vec!["movie".into()],
+        catalogs: vec![],
+        contact_email: None,
+        background: None,
+        logo: None,
+        id_prefixes: None,
+        description: None,
+    });
+    /*
+    // Returns a bad request every time we do that
+    app.middleware(
+        CorsMiddleware::new()
+            .allow_origin(CorsOrigin::from("*"))
+            .allow_methods(HeaderValue::from_static("GET, POST, OPTIONS")),
+    );
+    */
+    app.at("/*").get(handle_path);
+    app.run("127.0.0.1:8000").unwrap();
+}
