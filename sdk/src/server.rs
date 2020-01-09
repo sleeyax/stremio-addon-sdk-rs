e3cd31c4b4e6cd22a5a1e554c1972bf8360d158d
Sleeyax <yourd3veloper@gmail.com>
2020-01-09 21:37:52 +0100

    Migrate to hyper

--- sdk/src/server.rs
+++ sdk/src/server.rs
@@ -1,31 +1,13 @@
-use crate::router::BuilderWithHandlers;
-use tide::middleware::{Cors, Origin};
-use http::header::HeaderValue;
-use super::router::AddonRouter;
-use super::landing_template::landing_template;
-use super::cache_middleware::Cache;
+use hyper::Request;
+use hyper::server::conn::AddrStream;
+use hyper::service::{make_service_fn, service_fn_ok};
+use hyper::{Body};
+use hyper::server::Server;
+use hyper::rt::Future;
+use super::router::Router;
+use super::builder::BuilderWithHandlers;
 
-async fn handle_manifest(req: tide::Request<BuilderWithHandlers>) -> tide::Response {
-    tide::Response::new(200).body_json(req.state().handlers[0].get_manifest()).unwrap()
-}
-
-async fn handle_landing(req: tide::Request<BuilderWithHandlers>) -> tide::Response {
-    tide::Response::new(200)
-        .body_string(
-            landing_template(req.state().handlers[0].get_manifest())
-        )
-        .set_header("Content-Type", "text/html")
-}
-
-async fn handle_path(req: tide::Request<BuilderWithHandlers>) -> tide::Response {
-    let path = req.uri().path();
-    let resource_response = match req.state().handle(path) {
-        Some(r) => r,
-        None => return tide::Response::new(404)
-    };
-    tide::Response::new(200).body_json(&resource_response).unwrap()
-}
-
+#[derive(Clone)]
 pub struct ServerOptions {
     pub port: i16,
     pub cache_max_age: Option<i32>
@@ -40,21 +22,21 @@ impl Default for ServerOptions {
     }
 }
 
-pub async fn serve_http(builder: BuilderWithHandlers, options: ServerOptions) {
-    let mut app = tide::with_state(builder);
-    app.middleware(
-        Cors::new()
-            .allow_methods(HeaderValue::from_static("GET, POST, OPTIONS"))
-            .allow_origin(Origin::from("*"))
-            .allow_credentials(false)
-    );
-    if let Some(cache) = options.cache_max_age {
-        app.middleware(
-            Cache::new(cache)
-        );
-    }
-    app.at("/manifest.json").get(handle_manifest);
-    app.at("/").get(handle_landing);
-    app.at("/*").get(handle_path);
-    app.listen(format!("127.0.0.1:{}", options.port)).await.expect("Failed to start server");
+pub fn serve_http(build: BuilderWithHandlers, options: ServerOptions) {
+    let addr = format!("127.0.0.1:{}", options.port).parse().unwrap();
+    
+    let service = make_service_fn(move |_: &AddrStream| {
+        let router = Router::new(build.clone(), options.clone());
+        service_fn_ok(move |req: Request<Body>| {
+            router.route(req)
+        })
+    });
+
+    let server = Server::bind(&addr)
+        .serve(service)
+        .map_err(|e| eprintln!("server error: {}", e));
+
+    println!("Running on http://{}", addr);
+
+    hyper::rt::run(server)
 }
