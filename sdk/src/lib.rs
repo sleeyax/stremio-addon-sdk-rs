7ea62150532ec6fbab620586b841f80f607a7746
Sleeyax <yourd3veloper@gmail.com>
2020-01-10 14:47:08 +0100

    Export now.sh modules

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -2,3 +2,12 @@ pub mod builder;
 pub mod server;
 pub mod landing_template;
 pub mod router;
+pub mod export {
+    pub mod serverless {
+        pub mod now {
+            pub use now_lambda::Request;
+            pub use now_lambda::IntoResponse;
+            pub use now_lambda::error::NowError;
+        }
+    }
+}
