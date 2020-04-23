4ff8d62c9dc8ed9609cbbe2b4bda12b8aad1179a
Martin Kavík <martin@kavik.cz>
2020-04-23 12:09:15 +0200

    feat: IP in ServerOptions ; port from env var in example

--- sdk/src/server.rs
+++ sdk/src/server.rs
@@ -1,3 +1,4 @@
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use futures::MapErr;
 use hyper::Request;
 use hyper::server::conn::AddrStream;
@@ -11,20 +12,22 @@ use super::builder::BuilderWithHandlers;
 #[derive(Clone)]
 pub struct ServerOptions {
     pub port: u16,
-    pub cache_max_age: i32
+    pub cache_max_age: i32,
+    pub ip: IpAddr,
 }
 impl Default for ServerOptions {
     fn default() -> Self {
         Self {
             // cache 3 days
             cache_max_age: 24 * 3600 * 3,
-            port: 7070
+            port: 7070,
+            ip: IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
         }
     }
 }
 
 pub fn serve_http(build: BuilderWithHandlers, options: ServerOptions) {
-    let addr = format!("127.0.0.1:{}", options.port).parse().unwrap();
+    let addr = SocketAddr::new(options.ip, options.port);
     
     let service = make_service_fn(move |_: &AddrStream| {
         let router = Router::new(build.clone(), options.clone());
@@ -37,12 +40,14 @@ pub fn serve_http(build: BuilderWithHandlers, options: ServerOptions) {
         .serve(service)
         .map_err(|e| eprintln!("server error: {}", e));
 
-    println!("Running on http://{}", addr);
+    println!("Running on: {}", addr);
 
     hyper::rt::run(server)
 }
 
-pub fn serve_serverless(req: now_lambda::Request, build: BuilderWithHandlers, options: ServerOptions) -> Result<impl now_lambda::IntoResponse, now_lambda::error::NowError> {
+pub fn serve_serverless(
+    req: now_lambda::Request, build: BuilderWithHandlers, options: ServerOptions
+) -> Result<impl now_lambda::IntoResponse, now_lambda::error::NowError> {
     let router = Router::new(build, options);
     Ok(router.route(req))
 }
